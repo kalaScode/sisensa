3926ad84d4db32c3e60d05d17a3ab20f70e3be6d
Rizky Alif Ichwanto <rizkyalif.ichwanto@gmail.com>
2025-01-23 14:55:38 +0700

    sebelum ke api

--- resources/views/menu/persetujuan.blade.php
+++ resources/views/menu/persetujuan.blade.php
@@ -113,7 +113,7 @@
                                                 src="{{ $c->user->Avatar ? asset('storage/' . $c->user->Avatar) : 'storage/profil.jpg' }}"
                                                 alt="Foto Profil" />
                                         </div>
-                                        <div class="ml-4">
+                                        <div class="ml-6 items-center">
                                             <div class="text-sm font-medium text-gray-900">
                                                 {{ $c->user->name ?? 'Nama Tidak Ditemukan' }}
                                             </div>
